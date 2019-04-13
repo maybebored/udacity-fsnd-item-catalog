62af6217ff70de010efed0ac1e9d4cc1802a1a9f
maybebored <mayuselvaraja@gmail.com>
2019-04-13 21:13:32 +0800

    Fix style according to PEP-8

--- populate_db.py
+++ populate_db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
-from database_setup import Product,Base
+from database_setup import Product, Base
 
 engine = create_engine('sqlite:///itemcatalog.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -21,39 +21,51 @@ session = DBSession()
 
 
 # Create dummy products
-product1 = Product(title='Leather Bat', description='The perfect leather bat for whacking \'sixes\' right out of the ground.',
-             category='Cricket', created_on = datetime.now())
+product1 = Product(title='Leather Bat',
+                   description='The perfect leather bat for whacking' +
+                               '\'sixes\' right out of the ground.',
+                   category='Cricket', created_on=datetime.now())
 session.add(product1)
 session.commit()
 
-product2 = Product(title='Leather Ball',description='The perfect leather ball for bowling those \'Googlys\'.',
-             category='Cricket',created_on=datetime.now())
+product2 = Product(title='Leather Ball',
+                   description='The perfect leather ' +
+                               'ball for bowling those \'Googlys\'.',
+                   category='Cricket', created_on=datetime.now())
 session.add(product2)
 session.commit()
 
-product3 = Product(title='Kneepad',description='The perfect knee pads for protecting against Bret Lee\'s pacers.',
-             category='Cricket',created_on=datetime.now())
+product3 = Product(title='Kneepad',
+                   description='Knee pads to protect against Bret Lee.',
+                   category='Cricket', created_on=datetime.now())
 session.add(product3)
 session.commit()
 
-product4 = Product(title='Football Boots',description='Messi dreams about wearing these against his worst enemies.',
-             category='Football',created_on=datetime.now())
+product4 = Product(title='Football Boots',
+                   description='Messi dreams about' +
+                               ' wearing these against his worst enemies.',
+                   category='Football', created_on=datetime.now())
 session.add(product4)
 session.commit()
 
-product5 = Product(title='Supreme Football',description='It looks less than what it can do. Try kicking it.',
-             category='Football',created_on=datetime.now())
+product5 = Product(title='Supreme Football',
+                   description='It looks less than what it can do.' +
+                               'Try kicking it.',
+                   category='Football', created_on=datetime.now())
 session.add(product5)
 session.commit()
 
-product6 = Product(title='Silver Hockey Stick',description='India loves this so much they are trying hockey on ice now.',
-             category='Ice Hockey',created_on=datetime.now())
+product6 = Product(title='Silver Hockey Stick',
+                   description='India loves this so much they are trying' +
+                               'hockey on ice now.',
+                   category='Ice Hockey', created_on=datetime.now())
 session.add(product6)
 session.commit()
 
-product7 = Product(title='Heavy Metal Ball',description='Can\'t say more.',
-             category='Putshot',created_on=datetime(2018,1,1))
+product7 = Product(title='Heavy Metal Ball',
+                   description='Can\'t say more.',
+                   category='Putshot', created_on=datetime(2018, 1, 1))
 session.add(product7)
 session.commit()
 
-print("Updated database");
+print("Updated database")
